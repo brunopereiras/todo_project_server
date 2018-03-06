9a65df4c0a8d5fe650b2b2dbafa51d5a083b783a
Bruno Duarte Pereiras Santos <bruno.santos@timeneon.com.br>
2018-03-06 15:15:13 -0300

    Adicionado as validações para criar e editar uma tarefa.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
+
   belongs_to :category
   belongs_to :folder
+
+  validates :title, presence: true
+  validates :description, length: { maximum: 500 }
+  validates :category, presence: true
+  validates :folder, presence: true
+
 end
--- db/migrate/20180305161953_create_tasks.rb
+++ db/migrate/20180305161953_create_tasks.rb
@@ -1,12 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.string :description
-      t.boolean :done
-      t.datetime :due_date
-      t.references :category, foreign_key: true
-      t.references :folder, foreign_key: true
+      t.string :title, null: false
+      t.string :description, null: true
+      t.boolean :done, default: false
+      t.references :category, null: false, foreign_key: true
+      t.references :folder, null: false, foreign_key: true
 
       t.timestamps
     end
