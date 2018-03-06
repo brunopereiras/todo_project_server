2002c25e5fcbbb6c64f3a497e7237bd9ae2a59e5
Bruno Duarte Pereiras Santos <bruno.santos@timeneon.com.br>
2018-03-06 13:30:16 -0300

    Corrigido o layout do projeto.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'folders#index'
+
   resources :tasks
   resources :categories
   resources :folders
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
